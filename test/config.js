02bee0e86aec67356ba222e7f72ce683b686e596
Rob Dodson <lets.email.rob@gmail.com>
2012-05-29 17:40:29 -0700

    Near functional...

---
+++ test/config.js
@@ -0,0 +1,43 @@
+require.config({
+    // Base URL relative to the test runner
+    // Paths are relative to this
+    baseUrl: '../app/',
+    paths: {
+        // Testing libs
+        'chai'          : '../test/chai/chai',
+        'common'        : '../test/common',
+        'jquery'        : '../assets/js/libs/jquery',
+        'underscore'    : '../assets/js/libs/underscore',
+        'backbone'      : '../assets/js/libs/backbone',
+        'use'           : '../assets/js/libs/backbone'
+    },
+    use: {
+        backbone: {
+            deps: ['use!underscore', 'jquery'],
+            attach: 'Backbone'
+        },
+        underscore: {
+            attach: '_'
+        },
+        mocha: {
+            attach: 'mocha'
+        }
+    },
+    priority: [
+        'jquery',
+        'underscore',
+        'common'
+    ]
+});
+
+mocha.setup({
+    ui: 'bdd',
+    ignoreLeaks: true
+});
+
+// Protect from barfs
+console = window.console || function() {};
+
+var runMocha = function() {
+    mocha.run();
+};
--- test/test.example.js
+++ test/test.example.js
@@ -1,48 +1,10 @@
-define([
-  "namespace",
-
-  // Libs
-  "jquery",
-  "use!backbone",
-
-  // Modules
-  "modules/example"
-],
-
-function(namespace, $, Backbone, Example) {
-  var expect = chai.expect;
-  
-  describe('Example.Photo', function() {
-    describe('defaults', function() {
-      /*
-      // Use Sinon to replace jQuery's ajax method
-      // with a spy. This spy will also come with
-      // some success data.
-      beforeEach(function() {
-        sinon.stub($, 'ajax').yieldsTo('success', {
-          count: '100',
-          message: 'oh boy, 100 pickles!'
+define(function(require) {
+    var Example = require('modules/example');
+    
+    describe('Example', function() {
+        describe('Photo', function() {
+           var photo = new Example.Photo();
+           console.log(photo); 
         });
-      });
-
-      // Restor jQuery's ajax method to its
-      // original state
-      afterEach(function() {
-        $.ajax.restore();
-      });
-
-    */
-      it('should have the proper defaults', function(done) {
-        var photo = new Example.Photo();
-        expect(photo.get('src')).to.equal('/images/placeholder.jpg');
-        done();
-      });
     });
-  });
-
-  return {
-    name: 'test.example'
-  };
-
-});
-
+}); 
